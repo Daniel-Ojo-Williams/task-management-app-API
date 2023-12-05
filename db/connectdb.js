9092ba8145e2999da80dedebdf360d70fc569ab1
Ojo-williams Daniel <ojowilliamsdaniel@gmail.com>
2023-12-06 00:59:36 +0100

    added environmental variables

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,13 +1,14 @@
 // import { Client } from 'pg';
 import pkg from "pg";
 const { Pool } = pkg;
+import 'dotenv/config'
 
 const client = new Pool({
-  host: "localhost",
-  port: 5432,
-  database: "freakydmuse",
-  user: "freakydmuse",
-  password: "Owdanny400",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 export default client;
